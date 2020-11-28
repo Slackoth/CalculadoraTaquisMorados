4dbf2bf9e398467513334f72d9fd0b8aebcbb912
renemel <00174118@uca.edu.sv>
2020-11-27 22:32:13 -0600

    [test] operation functions

---
+++ test/operations.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double sum(double a, double b ){
+    return a+b;
+}
+
+double resta(double a, double b ){
+    return a-b;
+}
+double multiply(double a, double b ){
+    return a*b;
+}
+//agregar validaciones para div/0
+double divide(double a, double b ){
+    return a/b;
+}
+
+double exp(double a){
+    return exp(a);
+}
+
+double squareroot(double a ){
+    return sqrt(a);
+}
+
+double negate(double a ){
+    return a*-1;
+}
+
+double inversor(double a ){
+    return 1/a;
+}
+
+
